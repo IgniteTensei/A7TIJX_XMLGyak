24eb7340189ca0a3dcbe103430345d789833f146
Györki Ádám <37386030+IgniteTensei@users.noreply.github.com>
2023-11-08 11:31:06 +0100

    Update Main.java

--- A7TIJX_1108/DomParseA7TIJX/src/Main.java
+++ A7TIJX_1108/DomParseA7TIJX/src/Main.java
@@ -13,13 +13,16 @@ public class Main {
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
-            Document doc = db.parse("C:\\Users\\IgniteTensei\\Desktop\\Egyetem\\9\\XML\\A7TIJX_XMLGyak\\A7TIJX_0927\\a7tijx_orarend.xml");
+            Document doc = db.parse(
+                    "C:\\Users\\IgniteTensei\\Desktop\\Egyetem\\9\\XML\\A7TIJX_XMLGyak\\A7TIJX_0927\\a7tijx_orarend.xml"
+            );
+            
             NodeList orak = doc.getElementsByTagName("ora");
 
             for (int i = 0; i < orak.getLength(); i++){
                 NodeList orakGyerek = orak.item(i).getChildNodes();
                 NamedNodeMap orakAttr = orak.item(i).getAttributes();
-                
+
 
             }
         }
