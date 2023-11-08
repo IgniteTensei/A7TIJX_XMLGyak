66bcc417c56bc36f77785ad2e62188437826b421
Györki Ádám <37386030+IgniteTensei@users.noreply.github.com>
2023-11-08 10:50:30 +0100

    1108

--- A7TIJX_1108/DOMWriteA7TIJX/src/Main.java
+++ A7TIJX_1108/DOMWriteA7TIJX/src/Main.java
@@ -1,11 +1,10 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileOutputStream;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
