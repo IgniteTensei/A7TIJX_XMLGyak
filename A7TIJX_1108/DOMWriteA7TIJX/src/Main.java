0d8332bebaae42183c7a1a2aa95c02b8a5065d9e
Györki Ádám <37386030+IgniteTensei@users.noreply.github.com>
2023-11-22 11:13:54 +0100

    a

--- A7TIJX_1108/DOMWriteA7TIJX/src/Main.java
+++ A7TIJX_1108/DOMWriteA7TIJX/src/Main.java
@@ -1,12 +1,90 @@
-import java.io.FileOutputStream;
+import org.w3c.dom.*;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSOutput;
+import org.w3c.dom.ls.LSSerializer;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import java.io.FileOutputStream;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List lista = new ArrayList();
+
+        StringWriter stringWriter = new StringWriter();
+        String subscrXML=null;
+        DOMImplementationRegistry registry = null;
+        DOMImplementationLS impls = null;
+        LSOutput domOutput = null;
+        LSSerializer domWriter = null;
+
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(
+                    "C:\\Users\\IgniteTensei\\Desktop\\Egyetem\\9\\XML\\A7TIJX_XMLGyak\\A7TIJX_0927\\a7tijx_orarend.xml"
+            );
+
+            registry =  DOMImplementationRegistry.newInstance();
+            impls =  (DOMImplementationLS)registry.getDOMImplementation("LS");
+
+            NodeList orak = doc.getElementsByTagName("ora");
+
+            ArrayList<String> oraGyerekekNeve = new ArrayList<String>();
+
+            for (int i = 0; i < orak.item(0).getChildNodes().getLength(); i++){
+                if(!orak.item(0).getChildNodes().item(i).getNodeName().equals("#text")){
+                    String temp = orak.item(0).getChildNodes().item(i).getNodeName();
+                    oraGyerekekNeve.add(temp.substring(0,1).toUpperCase() + temp.substring(1,temp.length()));
+                }
+            }
+
+            for (int i = 0; i < orak.getLength(); i++){
+                NodeList orakAdatok = orak.item(i).getChildNodes();
+                NamedNodeMap orakAttr = orak.item(i).getAttributes();
+
+                int k = 0;
+                for (int j = 0; j < orakAdatok.getLength(); j++){
+                    if(!orakAdatok.item(j).toString().contains("   ")){
+                        Node node = orakAdatok.item(j);
+                        String gyerekNev = oraGyerekekNeve.get(k).toString();
+                        if(!gyerekNev.equals("Idopont")){
+                            System.out.println(gyerekNev + ": " + node.getTextContent());
+                        }
+                        else{
+                            System.out.println(gyerekNev + "(mettől meddig): " + node.getTextContent());
+                        }
+
+                        k++;
+                    }
+                }
+
+                k = 0;
+                System.out.println("---------");
+            }
+
+            domOutput = impls.createLSOutput();
+            domOutput.setEncoding("a7tijx_orarend1.xml");
+            domOutput.setCharacterStream(stringWriter);
+            domOutput.setEncoding("UTF-8");
+
+            domWriter = impls.createLSSerializer();
+            DOMConfiguration domConfig = domWriter.getDomConfig();
+            domConfig.setParameter("format-pretty-print", true);
+            domConfig.setParameter("element-content-whitespace", true);
+            domWriter.setNewLine("\r\n");
+            domConfig.setParameter("cdata-sections", Boolean.TRUE);
+
+            domWriter.write(doc, domOutput);
+            subscrXML = domOutput.getCharacterStream().toString();
+
+        }
+        catch (Exception ee) {
+            ee.printStackTrace();
+        }
     }
 }
--- A7TIJX_1108/DomParseA7TIJX/src/Main.java
+++ A7TIJX_1108/DomParseA7TIJX/src/Main.java
@@ -16,14 +16,42 @@ public class Main {
             Document doc = db.parse(
                     "C:\\Users\\IgniteTensei\\Desktop\\Egyetem\\9\\XML\\A7TIJX_XMLGyak\\A7TIJX_0927\\a7tijx_orarend.xml"
             );
-            
+
+            Document xmlout;
+
             NodeList orak = doc.getElementsByTagName("ora");
 
+            ArrayList<String> oraGyerekekNeve = new ArrayList<String>();
+
+            for (int i = 0; i < orak.item(0).getChildNodes().getLength(); i++){
+                if(!orak.item(0).getChildNodes().item(i).getNodeName().equals("#text")){
+                    String temp = orak.item(0).getChildNodes().item(i).getNodeName();
+                    oraGyerekekNeve.add(temp.substring(0,1).toUpperCase() + temp.substring(1,temp.length()));
+                }
+            }
+
             for (int i = 0; i < orak.getLength(); i++){
-                NodeList orakGyerek = orak.item(i).getChildNodes();
+                NodeList orakAdatok = orak.item(i).getChildNodes();
                 NamedNodeMap orakAttr = orak.item(i).getAttributes();
 
+                int k = 0;
+                for (int j = 0; j < orakAdatok.getLength(); j++){
+                    if(!orakAdatok.item(j).toString().contains("   ")){
+                        Node node = orakAdatok.item(j);
+                        String gyerekNev = oraGyerekekNeve.get(k).toString();
+                        if(!gyerekNev.equals("Idopont")){
+                            System.out.println(gyerekNev + ": " + node.getTextContent());
+                        }
+                        else{
+                            System.out.println(gyerekNev + "(mettől meddig): " + node.getTextContent());
+                        }
 
+                        k++;
+                    }
+                }
+
+                k = 0;
+                System.out.println("---------");
             }
         }
         catch (Exception ee) {
